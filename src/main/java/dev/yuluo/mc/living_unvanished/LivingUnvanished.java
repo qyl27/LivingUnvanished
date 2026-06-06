a1e9c0b9b803b860a750098d4664dcd7a5aea110
秋雨落 <i@rain.cx>
2026-06-06 10:56:51 +0800

    feat: create the world!
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ src/main/java/dev/yuluo/mc/living_unvanished/LivingUnvanished.java
@@ -0,0 +1,13 @@
+package dev.yuluo.mc.living_unvanished;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+
+@Mod(LivingUnvanished.MODID)
+public class LivingUnvanished {
+    public static final String MODID = "living_unvanished";
+
+    public LivingUnvanished(IEventBus modEventBus, ModContainer modContainer) {
+    }
+}
---
+++ src/main/java/dev/yuluo/mc/living_unvanished/LivingUnvanishedClient.java
@@ -0,0 +1,12 @@
+package dev.yuluo.mc.living_unvanished;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(value = LivingUnvanished.MODID, dist = Dist.CLIENT)
+public class LivingUnvanishedClient {
+    public LivingUnvanishedClient(IEventBus modEventBus, ModContainer container) {
+    }
+}
